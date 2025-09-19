ab2fb68c770a09f7658ffa4489679506ff0cc5c1
Inna-Khalas <innamakarec1@gmail.com>
2025-09-19 19:16:40 +0400

    fix: endless requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react'
-import './styles/App.scss'
-import CatalogList from './components/CatalogList/CatalogList'
-import type {ApiBook} from './types/book'
-import { fetchBooks } from './api/book'
+import { useEffect, useState } from 'react';
+import './styles/App.scss';
+import CatalogList from './components/CatalogList/CatalogList';
+import type { ApiBook } from './types/book';
+import { fetchBooks } from './api/book';
 
 function App() {
-  const [books, setBooks] = useState<ApiBook[]>([])
-  useEffect( ()=>{  async function loadBooks() {
-    const res = await fetchBooks("mongodb", 1);
-    setBooks(res.books); 
-  }
-  loadBooks();
-  })
+  const [books, setBooks] = useState<ApiBook[]>([]);
+  useEffect(() => {
+    async function loadBooks() {
+      const res = await fetchBooks('javascript', 1);
+      console.log(res);
+
+      setBooks(res.books);
+    }
+    loadBooks();
+  }, []);
 
   return (
     <>
-    <CatalogList books={books}/>
+      <CatalogList books={books} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
